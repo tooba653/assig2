6643308f0322f625fe4d8ea1010f726d15be6d41
tooba653 <164219929+tooba653@users.noreply.github.com>
2024-10-18 17:34:06 +0500

    first commit

---
+++ src/component/About.tsx
@@ -0,0 +1,8 @@
+function About() {
+    return (
+      <>
+        <h1><i> About Us</i></h1>
+        </>
+    );
+  }
+  export default About;
---
+++ src/component/Header.tsx
@@ -0,0 +1,8 @@
+function Header() {
+    return (
+      
+        <h1><b> Welcome To My Website</b></h1>
+      
+    );
+  }
+  export default Header;
